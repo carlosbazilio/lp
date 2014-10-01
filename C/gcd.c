b6ab8406f47def85d676fe09210cce0ec6155e17
Carlos Bazilio <carlosbazilio@gmail.com>
2014-09-30 22:40:32 -0300

    Commit inicial

--- C/gcd.c
+++ C/gcd.c
@@ -1,22 +1,17 @@
-#include "stdio.h"
+#include <signal.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int gcd(int x, int y) {
-	if ((x < 1) || (y < 1))
-		return 0;
-	else
-		if (x == y)
-			return x;
-		else
-			if (x < y)
-				return gcd(x, y-x);
-			else
-				return gcd(x-y, y);
+int gcd (int x, int y) {
+  if (y == 0)
+    return x;
+  else
+    return gcd(y, x % y);
 }
 
-main() {
-	int x, y;
-	printf("Entre com 2 valores: ");
-	scanf("%d%d", &x, &y);
-	printf("O maior divisor comum entre %d e %d eh %d\n", x, y, gcd(x,y));
+main()
+{
+  printf("%d\n", gcd(16,4));
 }
 
---
+++ ruby/testes.rb
@@ -0,0 +1,11 @@
+puts 'hello, world'
+language = 'Ruby'
+puts "hello, #{language}"
+language = 'my Ruby'
+puts "hello, #{language}"
+
+code = "Time.now"
+result = eval(code)
+puts result
+
+
