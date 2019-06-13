df1a9266b70dedef70332e831663ddd9107d7ba4
Carlos Bazilio <carlosbazilio@gmail.com>
2019-06-13 18:57:42 -0300

    Inserção de loop para recuperar mais de um link

--- scala/web-crawler.scala
+++ scala/web-crawler.scala
@@ -1,14 +1,33 @@
 import scala.io._
 import scala.util.matching.Regex
+import java.io.IOException
 
 object Crawler {
 	def getLinks (url : String) = {
-		val regex = new Regex("http://(\\w+\\.)*(\\w+)(\\d*)?")
-		val conteudo = Source.fromURL(url).mkString
-		regex.findAllIn(conteudo)
+		val regex = new Regex("http(s)?://(\\w+\\.)*(\\w+)(:\\d+)?")
+		var links = List[String]()
+		try {
+			val conteudo = Source.fromURL(url).mkString
+			links = regex.findAllIn(conteudo).toList
+		} catch {
+			case ex: IOException => {
+		    	println("Erro ao processar url: " + url)
+		 	}
+		}
+		links
 	}
 }
 
-val links = Crawler.getLinks(args(0))
-links.foreach { println }
+var links = List(args(0))
+var qtd_acessos = 10;
+while (links.size > 0 && qtd_acessos > 0) {
+	val link = links.head
+	println("Processando: " + link)
+	links = (links.tail ++ Crawler.getLinks(link)).distinct
+	qtd_acessos = qtd_acessos - 1
+}
+
+println("Resultado: ")
+links.distinct.foreach { println }
+//links.foreach { println }
 
