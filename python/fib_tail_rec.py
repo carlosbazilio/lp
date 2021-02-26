738a2b2af7d2652d1e15bb89f9bd5e7c097d60f4
bazilio <carlosbazilio@gmail.com>
2021-02-26 13:07:20 -0300

    Aninhamento de função para melhor qualidade do código

--- python/fib_tail_rec.py
+++ python/fib_tail_rec.py
@@ -1,11 +1,11 @@
-def fib_aux (x, ant, curr):
-    if (x == 1):
-        return ant
-    return fib_aux (x-1, curr, ant+curr)
 
 def fib (x):
+	def fib_aux (x, ant, curr):
+	    if (x == 1):
+	        return ant
+	    return fib_aux (x-1, curr, ant+curr)
 	return fib_aux (x, 0, 1)
 
 import sys
 
-print fib(int(sys.argv[1]))
+print (fib(int(sys.argv[1])))
