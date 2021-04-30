918b9805522312644f2b8f60f21cf491cb775253
bazilio <carlosbazilio@gmail.com>
2021-04-30 12:29:55 -0300

    Versão inicial

---
+++ haskell/datalivro.hs
@@ -0,0 +1,27 @@
+-- data Pub = Livro Integer [Char] [Char] Double Double deriving (Show)
+
+data Pub = Livro { cod :: Integer, 
+                   titulo :: [Char], 
+                   autor :: [Char],
+                   preco :: Double,
+                   qtd :: Double
+                 } deriving (Show)    
+
+livros :: [Pub]
+livros = [Livro 1 "Eu sou Malala" "Malala Yousafzai" 50.0 2.0, 
+          Livro 2 "Engenharia de Software" "Ian Sommerville" 200.0 4.0, 
+          Livro 3 "O Principe" "Maquiavel" 150.0 1.0, 
+          Livro 4 "The Atrocity Archives" "Charles Stross" 300.0 2.0]
+
+-- getPreco :: Pub -> Double
+-- getPreco (Livro id titulo autor preco qtd) = preco
+
+-- getQtd :: Pub -> Double
+-- getQtd (Livro id titulo autor preco qtd) = qtd
+
+-- getValorEstoque :: Pub -> Double
+-- getValorEstoque (Livro id titulo autor preco qtd) = preco * qtd
+
+-- somalivros :: [Pub] -> Double
+-- somalivros lista = foldl (\x y -> x + getValorEstoque y) 0 lista
+
---
+++ haskell/tuplaslivro.hs
@@ -0,0 +1,23 @@
+type Livro = (Integer, [Char], [Char], Double, Double)
+
+livros :: [Livro]
+livros = [(1,"Eu sou Malala","Malala Yousafzai",50.0,2.0),
+          (2,"Engenharia de Software","Ian Sommerville",200.0,4.0),
+          (3,"O Principe","Maquiavel",150.0,1.0),
+          (4,"The Atrocity Archives","Charles Stross",300.0,2.0)]
+
+getPreco :: Livro -> Double
+getPreco (id, titulo, autor, preco, qtd) = preco
+
+getQtd :: Livro -> Double
+getQtd (id, titulo, autor, preco, qtd) = qtd
+
+getValorEstoque :: Livro -> Double
+getValorEstoque (id, titulo, autor, preco, qtd) = preco * qtd
+
+somalivros :: [Livro] -> Double
+somalivros lista = foldl (\x y -> x + getValorEstoque y) 0 lista
+
+main = 
+    print (foldl (\x (id1, titulo1, autor1, preco1, qtd1) -> (preco1 * qtd1) + x) 0 livros)
+
