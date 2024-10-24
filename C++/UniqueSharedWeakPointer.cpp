72465d5d28574e98a51835a7faf26f6f4f7356a5
bazilio <carlosbazilio@gmail.com>
2024-10-24 17:32:35 -0300

    Atualização do exemplo para incluir o raw pointer

--- C++/UniqueSharedWeakPointer.cpp
+++ C++/UniqueSharedWeakPointer.cpp
@@ -22,8 +22,13 @@ public:
 };
 
 int main() {
-
 	//////////////////////////////////////////////
+	// RAW POINTER
+	Rectangle* P = new Rectangle(10, 5);
+	cout << P->area() << endl; // This'll print 50
+	delete P; // This command must inserted by the programmer. Otherwise a garbage will be created.
+
+	/*/////////////////////////////////////////////
 	// UNIQUE
 	unique_ptr<Rectangle> P1(new Rectangle(10, 5));
 	cout << P1->area() << endl; // This'll print 50
@@ -34,6 +39,8 @@ int main() {
 
 	// This'll print 50
 	cout << P2->area() << endl;
+
+	// cout << P1->area() << endl; // This'll cause a segmentation fault because the move assignment operator (line 33) transferred the ownership of the area point out by P1 to P2
 
 	//////////////////////////////////////////////
 	// SHARED
@@ -52,6 +59,7 @@ int main() {
  
     // This'll also print 50 now
     // This'll print 2 as Reference Counter is 2
+    // use_count() is a property only available with shared_ptr type
     cout << P3.use_count() << endl;	
 
 	//////////////////////////////////////////////
@@ -59,7 +67,7 @@ int main() {
 	weak_ptr<Rectangle> P5 = P3;
 
     // This'll still print 2 as Reference Counter is 2
-    cout << P5.use_count() << endl;	
+    cout << P5.use_count() << endl; */
     
 	return 0;
 }
